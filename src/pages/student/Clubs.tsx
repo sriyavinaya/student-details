3d62dc9130af6623ebd9ce4f559a3fe5b15daffe
sriyavinaya <sriyavinayakoppada@gmail.com>
2025-03-20 08:49:55 +0530

    Delete

--- src/pages/student/Clubs.tsx
+++
@@ -1,8 +0,0 @@
-
-import PageTemplate from "@/components/student/StudentPageTemplate";
-
-const Clubs = () => {
-  return <PageTemplate title="Clubs and Societies" />;
-};
-
-export default Clubs;
--- src/pages/student/Sports.tsx
+++
@@ -1,8 +0,0 @@
-
-import PageTemplate from "@/components/student/StudentPageTemplate";
-
-const Sports = () => {
-  return <PageTemplate title="Sports" />;
-};
-
-export default Sports;
